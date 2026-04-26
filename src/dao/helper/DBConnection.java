a14e94ae236de15d6ae5db2c41b6383ce2878222
huynhducha <huynhducha910@gmail.com>
2026-04-26 23:35:35 +0700

    refactor: cap nhat lai dia chi URL

--- src/dao/helper/DBConnection.java
+++ src/dao/helper/DBConnection.java
@@ -8,18 +8,18 @@ public class DBConnection {
     private static Connection connection = null;
 
     // Thay đổi các thông số này theo Database thực tế của bạn
-    private static final String URL = "jdbc:sqlserver://localhost:3306;databaseName=AppointmentDB";
-    private static final String USER = "sa";
+    private static final String URL = "jdbc:mysql://localhost:3306/AppointmentDB?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8";
+    private static final String USER = "root";
     private static final String PASS = "huynhducha";
 
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
             try {
                 // Đảm bảo Driver đã được load
-                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                Class.forName("com.mysql.cj.jdbc.Driver");
                 connection = DriverManager.getConnection(URL, USER, PASS);
             } catch (ClassNotFoundException e) {
-                throw new SQLException("Không tìm thấy Driver JDBC!");
+                throw new SQLException("Không tìm thấy Driver JDBC cho MySQL!");
             }
         }
         return connection;
